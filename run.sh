a85474a6780a3effff2e8104c5d1a5a69c9ed37d
Namhyeon, Go <gnh1201@gmail.com>
2017-01-31 15:09:15 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,4 @@
+mysqld &
+
+echo "my host is: ${PUBLIC_ADDRESS}"
+${JBOSS_HOME}/bin/run.sh -b 0.0.0.0 -Djava.rmi.server.hostname=${PUBLIC_ADDRESS} -Dremoting.bind_by_host=false -Djava.rmi.server.useLocalHostname=false
